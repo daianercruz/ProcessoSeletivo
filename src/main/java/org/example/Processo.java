c221b14a21cb1e01b4c8bacc3371db6a5559ebf2
Daiane Cruz <daianercruz12@gmail.com>
2024-05-08 09:50:57 -0300

    Criando selecao de candidatos

--- src/main/java/org/example/Processo.java
+++ src/main/java/org/example/Processo.java
@@ -1,12 +1,40 @@
 package org.example;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Processo {
 
     public static void main (String[]args){
-        analisarCandidato (1900.0);
+       /* analisarCandidato (1900.0);
         analisarCandidato (2200.0);
-        analisarCandidato (2000.0);
+        analisarCandidato (2000.0);*/
+
+        SelecaoCandidatos();
     }
+
+    static void SelecaoCandidatos(){
+        String [] candidatos = {"Felipe", "Marcia", "Juliana", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela",
+                "Daniela", "Jorge"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " +candidato + " solicitou este valor de salario:" + salarioPretendido);
+            if (salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " Foi selecionado para vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+
     static void analisarCandidato (double salarioPretendido){
 
         double salarioBase = 2000.0;
